e4ea1722189351bf737d8aafc8d2286e34c2dcab
Jakub Krok <50359069+Brtgkrk@users.noreply.github.com>
2023-11-08 15:14:36 +0100

    Cors policy support added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
@@ -8,6 +9,7 @@ const sensorDataRouter = require('./routes/sensorData');
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
